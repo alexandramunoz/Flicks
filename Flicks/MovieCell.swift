2f47fcb8819cad37b1b72a93c686129b43d46648
alexandramunoz <amunoz@ufl.edu>
2016-01-11 00:10:35 -0500

    I have completed the work from the first 3 Assignment QuickStart videos.

---
+++ Flicks/MovieCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieCell.swift
+//  Flicks
+//
+//  Created by Alexandra Munoz on 1/9/16.
+//  Copyright © 2016 Alexandra Munoz. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
