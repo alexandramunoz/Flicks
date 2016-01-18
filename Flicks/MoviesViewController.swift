61f6da250b0abda63d06983750827d6b11311e08
alexandramunoz <amunoz@ufl.edu>
2016-01-17 20:01:26 -0500

    I have completed the requirements for the Flicks app.

--- Flicks/MoviesViewController.swift
+++ Flicks/MoviesViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import AFNetworking
+import EZLoadingActivity
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var refreshControl: UIRefreshControl!
 
     @IBOutlet weak var tableView: UITableView!
     
@@ -17,8 +20,14 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "onRefresh", forControlEvents: UIControlEvents.ValueChanged)
+        tableView.insertSubview(refreshControl, atIndex: 0)
+        
         tableView.dataSource = self
         tableView.delegate = self
+        
         
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
         let url = NSURL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
@@ -38,7 +47,11 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                             
                             self.movies = responseDictionary["results"] as? [NSDictionary]
                             self.tableView.reloadData()
+                            EZLoadingActivity.hide(success: true, animated: true)
                     }
+                }
+                else {
+                    EZLoadingActivity.hide(success: false, animated: true)
                 }
         });
         task.resume()
@@ -51,6 +64,9 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         // Dispose of any resources that can be recreated.
     }
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        EZLoadingActivity.showWithDelay("Loading...", disableUI: true, seconds: 2)
+        
         if let movies = movies {
             return movies.count
         } else{
@@ -64,13 +80,35 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let movie = movies![indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
+        let posterPath = movie["poster_path"] as! String
+        
+        let baseUrl = "http://image.tmdb.org/t/p/w500"
+        
+        let imageUrl = NSURL(string: baseUrl + posterPath)
+        
         
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
+        cell.posterView.setImageWithURL(imageUrl!)
         
         print("row \(indexPath.row)")
+        
         return cell
         
+    }
+    func delay(delay:Double, closure:()->()) {
+        dispatch_after(
+            dispatch_time(
+                DISPATCH_TIME_NOW,
+                Int64(delay * Double(NSEC_PER_SEC))
+            ),
+            dispatch_get_main_queue(), closure)
+    }
+    
+    func onRefresh() {
+        delay(2, closure: {
+            self.refreshControl.endRefreshing()
+        })
     }
 
     /*
