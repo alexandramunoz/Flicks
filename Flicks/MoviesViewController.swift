cfc60b2ac2c62ed88f6daecf9b04e43fb979d6d4
alexandramunoz <amunoz@ufl.edu>
2016-01-17 23:12:30 -0500

    Poster images now fade in

--- Flicks/MoviesViewController.swift
+++ Flicks/MoviesViewController.swift
@@ -15,8 +15,9 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
 
     @IBOutlet weak var tableView: UITableView!
     
-    @IBOutlet weak var searchBar: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
     var movies : [NSDictionary]?
+
     var filteredData: [String]!
     
    
@@ -30,7 +31,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         tableView.dataSource = self
         tableView.delegate = self
-        searchBar.delegate = self
         
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
         let url = NSURL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
@@ -89,6 +89,36 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         let imageUrl = NSURL(string: baseUrl + posterPath)
         
+        let imageStringUrl = baseUrl + posterPath
+        let imageUrlRequest = NSURLRequest(URL: NSURL(string: imageStringUrl)!)
+        
+        let imageRequest = NSURLRequest(URL: NSURL(string: imageStringUrl)!)
+        cell.posterView.setImageWithURLRequest(
+            imageUrlRequest,
+            placeholderImage: nil,
+            success: { (imageUrl, imageResponse, image) -> Void in
+                
+                // imageResponse will be nil if the image is cached
+                if imageResponse != nil {
+                    print("Image was NOT cached, fade in image")
+                    cell.posterView.alpha = 0.0
+                    cell.posterView.image = image
+                    UIView.animateWithDuration(0.4, animations: { () -> Void in
+                        cell.posterView.alpha = 1.0
+                    
+                    })
+                } else {
+                    print("Image was cached so just update the image")
+                    cell.posterView.image = image
+                }
+            },
+            
+            failure: { (imageRequest, imageResponse, error) ->
+                Void in
+                // do something for the failure condition
+                
+        })
+
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
         cell.posterView.setImageWithURL(imageUrl!)
